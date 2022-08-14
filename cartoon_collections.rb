5aeeacbed9ed804bf5f815cde223fa31932ae3b1
andtrno1 <andtrno1@gmail.ccom>
2022-08-14 19:47:44 -0400

    Completed Assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do |word|
+    word[0] = word[0].upcase
+    word << "!"
+  end
+  return calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  ans = calls.any? do |call|
+    call.length > 4
+  end
+  return ans
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  return foods.detect do |food|
+    cheese_types.include?(food)
+  end
 end
